22b41af7c3378381e3a1f435b8a46d9f079a5171
Si Wu <sw.si.wu@gmail.com>
2018-10-15 20:18:36 -0700

    add some js for reminder button

---
+++ main.js
@@ -0,0 +1,27 @@
+$("#reminder").click( function() {
+    var to_label = document.createElement("label");
+    to_label.innerHTML("To");
+    $(".email_area").append(to_label);
+    
+    var to_textbox = document.createElement("textarea");
+    to_label.after(to_textbox);
+    
+   /* var title_label = 
+    
+    var content_label = 
+    var submit_button = */
+});
+
+
+/*
+$("#title_only").click( function() {
+    
+    
+});
+
+$("#blank_email").click( function() {
+    
+    
+}); */
+
+
